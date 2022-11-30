e5848bc81f48eafafa29cd6ac8412521d420b852
Pawan arora <pawan.arora@logicielsolutions.co.in>
2022-11-30 10:14:51 +0530

    Testing Pass case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ci_cd_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test Pass Case', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
